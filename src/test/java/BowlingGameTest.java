1c6598566c567a515a90a563dd3dd7715843e215
maru <marielacagnoli@gmail.com>
2019-11-27 14:43:56 -0300

    junit

---
+++ src/test/java/BowlingGameTest.java
@@ -0,0 +1,48 @@
+import BowlingGame.BowlingGame;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BowlingGameTest {
+
+    private BowlingGame bowlingGame;
+
+    @Before
+    public void setUp() throws Exception {
+     bowlingGame = new BowlingGame();
+    }
+
+    @Test
+    public void testGutterGame() throws Exception {
+
+        rollMany(20,0);
+
+        assertEquals(0,bowlingGame.score());
+    }
+
+    @Test
+    public void testAllOnes() throws Exception {
+        rollMany(20,1);
+        assertEquals(20, bowlingGame.score());
+    }
+
+    @Test
+    public void testOneSpare() throws Exception {
+        rollSpare();
+        bowlingGame.roll(3);
+        rollMany(17, 0);
+        assertEquals(16, bowlingGame.score());
+
+    }
+
+    public void rollMany(int n, int pins) {
+        for(int i= 0; i < n; i++){
+            bowlingGame.roll(pins);
+        }
+    }
+    private void rollSpare(){
+        bowlingGame.roll(5);
+        bowlingGame.roll(5);
+    }
+}
